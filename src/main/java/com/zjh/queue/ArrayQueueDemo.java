fc2d638b2ae567f31fd0660745dfb9eb0a5a64ea
nevermore722 <45936418+nevermore722@users.noreply.github.com>
2019-11-23 18:42:09 +0800

    :tada: 初始化提交。数组模拟队列代码完成。

---
+++ src/main/java/com/zjh/queue/ArrayQueueDemo.java
@@ -0,0 +1,105 @@
+package com.zjh.queue;
+
+/**
+ * @author ：ZouJiaHui
+ * @date ：Created in 2019/11/23 18:20
+ * @description：
+ * @modified By：
+ * @version: 1.0
+ */
+public class ArrayQueueDemo {
+    public static void main(String[] args) {
+
+    }
+}
+
+//使用数组模拟队列-编写一个ArrayQueue类
+class ArrayQueue {
+    /**
+     * 表示数组的最大容量
+     */
+    private int maxSize;
+
+    /**
+     * 队列头
+     */
+    private int front;
+
+    /**
+     * 队列尾部
+     */
+    private int rear;
+
+    /**
+     * 该数据用于存放数据，模拟队列
+     */
+    private int[] arr;
+
+    /**
+     * 创建队列的构造器
+     *
+     * @param arrMaxSize
+     */
+    public ArrayQueue(int arrMaxSize) {
+        maxSize = arrMaxSize;
+        arr = new int[maxSize];
+        //指向队列头部，分析出front是指向队列头的前一个位置
+        front = -1;
+        //指向队列尾，指向队列尾的数据（即就是队列最后一个数据）
+        rear = -1;
+    }
+
+    //判断队列是否满
+    public boolean isFull() {
+        return rear == maxSize - 1;
+    }
+
+    //判断队列是否为空
+    public boolean isEmpty() {
+        return rear == front;
+    }
+
+    //添加数据到队列
+    public void addQueue(int n) {
+        //判断队列是否满
+        if (isFull()) {
+            System.out.println("队列满，不能加入数据~");
+            return;
+        }
+        //让rear后移
+        rear++;
+        arr[rear] = n;
+    }
+
+    //获取队列的数据，出队列
+    public int getQueue() {
+        //判断队列是否空
+        if (isEmpty()) {
+            //通过抛出异常
+            throw new RuntimeException("队列空，不能取数据");
+        }
+        front++;
+        return arr[front];
+    }
+
+    //显示队列的所有数据
+    public void showQueue() {
+        //遍历
+        if (isEmpty()) {
+            System.out.println("队列空的，没有数据~~");
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.printf("arr[%d]=%d\n", i, arr[i]);
+        }
+    }
+
+    //显示队列的头数据，注意不是取出数据
+    public int headQueue() {
+        //判断
+        if (isEmpty()) {
+            throw new RuntimeException("队列空的，没有数据~~");
+        }
+        return arr[front + 1];
+    }
+}
