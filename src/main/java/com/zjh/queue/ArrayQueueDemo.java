7926d8a5e6192033e7cc8e98e33131fd17bf703c
nevermore722 <45936418+nevermore722@users.noreply.github.com>
2019-11-23 21:47:32 +0800

    :ok_hand: 更新代码。数组模拟队列代码添加测试的代码。

--- src/main/java/com/zjh/queue/ArrayQueueDemo.java
+++ src/main/java/com/zjh/queue/ArrayQueueDemo.java
@@ -1,5 +1,7 @@
 package com.zjh.queue;
 
+import java.util.Scanner;
+
 /**
  * @author ：ZouJiaHui
  * @date ：Created in 2019/11/23 18:20
@@ -9,7 +11,59 @@ package com.zjh.queue;
  */
 public class ArrayQueueDemo {
     public static void main(String[] args) {
-
+        //测试一把
+        //创建一个队列
+        ArrayQueue arrayQueue = new ArrayQueue(3);
+        //接受用户输入
+        char key = ' ';
+        Scanner scanner = new Scanner(System.in);
+        boolean loop = true;
+        //输出一个菜单
+        while (loop) {
+            System.out.println("s(show):显示队列");
+            System.out.println("e(exit):退出程序");
+            System.out.println("a(add):添加数据到队列");
+            System.out.println("g(get):从队列取出数据");
+            System.out.println("h(head):查看队列头的数据");
+            //接收一个字符
+            key = scanner.next().charAt(0);
+            switch (key) {
+                case 's':
+                    arrayQueue.showQueue();
+                    break;
+                case 'a':
+                    System.out.println("输出一个数");
+                    int value = scanner.nextInt();
+                    arrayQueue.addQueue(value);
+                    break;
+                //取出数据
+                case 'g':
+                    try {
+                        int res = arrayQueue.getQueue();
+                        System.out.printf("取出的数据是%d\n", res);
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                //查看队列头的数据
+                case 'h':
+                    try {
+                        int res = arrayQueue.headQueue();
+                        System.out.printf("队列头的数据是%d\n",res);
+                    }catch (Exception e){
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                //退出
+                case 'e':
+                    scanner.close();
+                    loop = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+        System.out.println("程序退出~~~");
     }
 }
 
