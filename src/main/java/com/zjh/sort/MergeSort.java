c74cbbe363f16b923141bdb555eec69233b50cbf
nevermore722 <45936418+nevermore722@users.noreply.github.com>
2020-02-12 11:52:25 +0800

    :tada: Initial commit.归并排序算法速度测试

--- src/main/java/com/zjh/sort/MergeSort.java
+++ src/main/java/com/zjh/sort/MergeSort.java
@@ -1,6 +1,8 @@
 package com.zjh.sort;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
 
 /**
  * @author ：ZouJiaHui
@@ -12,12 +14,29 @@ import java.util.Arrays;
 public class MergeSort {
 
   public static void main(String[] args) {
-    int arr[] = {8, 4, 5, 7, 1, 3, 6, 2, 0, 234};
+    //8->merge 7 ，80000->mergr 80000-1 冒泡80000 * O(n^2)
+//    int arr[] = {8, 4, 5, 7, 1, 3, 6, 2, 0, 234};
+    //创建一个80000个的随机数组
+    int[] arr = new int[8000000];
+    for (int i = 0; i < 8000000; i++) {
+      //生成一个[0,8000000)数
+      arr[i] = (int) (Math.random() * 8000000);
+    }
+
+    System.out.println("排序前");
+    Date date1 = new Date();
+    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    String date1Str = simpleDateFormat.format(date1);
+    System.out.println("排序前的时间是=" + date1Str);
+
     //归并排序需要一个额外空间
     int temp[] = new int[arr.length];
     mergeSort(arr, 0, arr.length - 1, temp);
-
-    System.out.println("归并排序后=" + Arrays.toString(arr));
+    System.out.println("排序后");
+    Date date2 = new Date();
+    String date2Str = simpleDateFormat.format(date2);
+    System.out.println("排序后的时间是=" + date2Str);
+//    System.out.println("归并排序后=" + Arrays.toString(arr));
   }
 
   //分+合的方法
@@ -45,7 +64,7 @@ public class MergeSort {
    * @param temp 做中转的数组
    */
   public static void merge(int[] arr, int left, int mid, int right, int[] temp) {
-    System.out.println("xxxx");
+//    System.out.println("xxxx");
     //c初始化i，左边有序序列的初始索引
     int i = left;
     //初始化j，右边有序序列的初始索引
@@ -93,7 +112,7 @@ public class MergeSort {
     t = 0;
     //
     int tempLeft = left;
-    System.out.println("tempLeft=" + tempLeft + " right=" + right);
+//    System.out.println("tempLeft=" + tempLeft + " right=" + right);
     while (tempLeft <= right) {
       //第一次合并tempLeft = 0，right = 1 //templeft = 2 right = 3 //tL = 0 ri = 3
       //最后一次tempLeft = 0，right = 7
