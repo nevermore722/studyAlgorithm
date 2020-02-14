21a197da39c6f0da366dba380363c2ffd1d68f3e
nevermore722 <45936418+nevermore722@users.noreply.github.com>
2020-02-14 11:59:49 +0800

    :tada: Initial commit.基数排序算法完成

--- src/main/java/com/zjh/sort/RadixSort.java
+++ src/main/java/com/zjh/sort/RadixSort.java
@@ -19,8 +19,21 @@ public class RadixSort {
 
   //基数排序方法
   public static void radixSort(int[] arr) {
-    //第一轮(针对每个元素的个位进行排序处理)
 
+    //根据前面的推导过程，我们可以得到最终的基数排序代码
+
+    //1.得到数组中最大的数的位数
+    //假设第一数就是最大数
+    int max = arr[0];
+    for (int i = 1; i < arr.length; i++) {
+      if (arr[i] > max) {
+        max = arr[i];
+      }
+    }
+    //得到最大数是几位数
+    int maxLength = (max + "").length();
+
+    //第一轮(针对每个元素的个位进行排序处理)
     //定义一个二维数组，表示10个桶，每个桶就是一个一维数组
     //说明
     //1.二维数组包含10个一维数组
@@ -33,7 +46,35 @@ public class RadixSort {
     //比如：bucketElementCounts[0],记录的就是bucket[0]桶的放入数据的个数
     int[] bucketElementCounts = new int[10];
 
-    //第1轮（针对每个元素的个位数进行排序处理）
+    //这里我们使用循环将代码处理
+    for (int i = 0, n = 1; i < maxLength; i++, n *= 10) {
+      //针对每个元素的对应位进行排序处理，第一次是个位，第二次是十位，第三次是百位。。
+      for (int j = 0; j < arr.length; j++) {
+        //取出每个元素对应位的值
+        int digitOfElement = arr[j] / n % 10;
+        //放入到对应的桶中
+        bucket[digitOfElement][bucketElementCounts[digitOfElement]] = arr[j];
+        bucketElementCounts[digitOfElement]++;
+      }
+      //按照这个桶的顺序（一维数组的下标依次取出数组，放入原来数组）
+      int index = 0;
+      //遍历每一个桶，并将桶中的数据，放入到原数组
+      for (int k = 0; k < bucketElementCounts.length; k++) {
+        //如果桶中，有数据，我们才放入到原数组
+        if (bucketElementCounts[k] != 0) {
+          //循环该桶即第k个桶（即第k个一维数组）,放入
+          for (int l = 0; l < bucketElementCounts[k]; l++) {
+            //取出元素放入到arr
+            arr[index++] = bucket[k][l];
+          }
+        }
+        //第i+1轮处理后，需要将每个bucketElementCounts[k] = 0！！！！！！！！！
+        bucketElementCounts[k] = 0;
+      }
+      System.out.println("第" + (i + 1) + "轮，对个位的排序处理 arr = " + Arrays.toString(arr));
+    }
+
+    /*//第1轮（针对每个元素的个位数进行排序处理）
     for (int j = 0; j < arr.length; j++) {
       //取出每个元素的个数
       int digitOfElement = arr[j] % 10;
@@ -104,7 +145,7 @@ public class RadixSort {
         }
       }
     }
-    System.out.println("第3轮，对百位的排序处理 arr = " + Arrays.toString(arr));
+    System.out.println("第3轮，对百位的排序处理 arr = " + Arrays.toString(arr));*/
   }
 
 }
