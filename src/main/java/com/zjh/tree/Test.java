15d74bd7836a03bedabe92f54623829a387e82b4
nevermore722 <45936418+nevermore722@users.noreply.github.com>
2020-02-21 12:07:13 +0800

    :tada: Initial commit.提交一个没啥用的文件

---
+++ src/main/java/com/zjh/tree/Test.java
@@ -0,0 +1,23 @@
+package com.zjh.tree;
+
+import java.util.ArrayList;
+
+/**
+ * @author ：ZouJiaHui
+ * @date ：Created in 2020/2/21 11:10
+ * @description：
+ * @modified By：
+ * @version: 1.0
+ */
+public class Test {
+
+  public static void main(String[] args) {
+    //以ArrayList 为例，看看是怎样进行数组扩容
+    //ArrayList 维护了数组 transient Object[] elementData; /
+    /**
+     * ArrayList 底层仍然是进行数组扩容
+     */
+    ArrayList arrayList = new ArrayList();
+
+  }
+}
